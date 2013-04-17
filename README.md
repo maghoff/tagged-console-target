af8fb307c9a994d8ed5c2d967dcef507781dedd1
Magnus Hoff <maghoff@gmail.com>
2013-04-17 16:53:05 +0200

    It works\! I can't believe it\!

---
+++ README.md
@@ -0,0 +1,44 @@
+TaggedConsoleTarget
+===================
+
+A log target for Winston that expects tagged log messages and writes them with lots of colors to the console or a given output stream.
+
+This module is designed to work together with [`TaggedLogger`](https://npmjs.org/package/tagged-logger).
+
+Install
+-------
+
+    npm install winston tagged-console-target
+
+Example
+-------
+
+	var winston = require('winston');
+	var TaggedConsoleTarget = require('tagged-console-target');
+
+	var logger = new (winston.Logger)({
+		transports: [
+			new TaggedConsoleTarget()
+		]
+	});
+
+	logger.info("Log message 1", {tags: ['module a']});
+	logger.info("Log message 2", {tags: ['module a', 'submodule a1']});
+	logger.info("Log message 3", {tags: ['module b']});
+
+
+Example output:
+
+	04:46:06.004 2013-04-17 Wednesday
+	04:46:06.007 [module a] Log message 1
+	04:46:06.008 [module a, submodule a1] Log message 2
+	04:46:06.008 [module b] Log message 3
+
+Except the output is in *glorious* color!
+
+Options
+-------
+
+`TaggedConsoleTarget` accepts one option:
+
+ * **target**: The target output stream. Defaults to `process.stdout`.
---
+++ index.js
@@ -0,0 +1,44 @@
+var util = require('util');
+var winston = require('winston');
+var moment = require('moment');
+
+function TaggedConsoleTarget(options) {
+	options = options || {};
+
+	this.name = 'taggedConsoleLogger';
+	this.level = options.level || 'info';
+	this.target = options.target || process.stdout;
+
+	this.prevTimestamp = new Date();
+	this.target.write(moment(this.prevTimestamp).format('hh:mm:ss.SSS YYYY-MM-DD dddd').grey + '\n');
+};
+util.inherits(TaggedConsoleTarget, winston.Transport);
+
+TaggedConsoleTarget.prototype.log = function (level, msg, meta, callback) {
+	var spec = {
+		'info': {
+			color: "white",
+		},
+		'warn': {
+			color: "yellow"
+		},
+		'error': {
+			color: "red"
+		}
+	};
+	var color = spec[level].color;
+
+	meta = meta || {};
+	var tags = meta.tags || [];
+	var timestamp = meta.timestamp || new Date();
+
+	if (moment(timestamp).format('YYYY-MM-DD') !== moment(this.prevTimestamp).format('YYYY-MM-DD')) {
+		this.prevTimestamp = timestamp;
+		this.target.write(moment(timestamp).format('hh:mm:ss.SSS YYYY-MM-DD dddd').grey + '\n');
+	}
+
+	this.target.write(moment(timestamp).format('hh:mm:ss.SSS').grey + (' [' + tags.join(', ') + '] ').green + msg[color] + '\n');
+	callback(null, true);
+};
+
+module.exports = TaggedConsoleTarget;
